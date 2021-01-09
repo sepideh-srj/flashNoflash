4a5bc1c6d782409b6821e2b9d3db6c30660d362f
smh31 <smh31@solar.planets>
2021-01-08 19:48:21 -0800

    Nothing

--- train.sh
+++ train.sh
@@ -18,5 +18,5 @@ conda activate pix2pix
 hostname
 echo ------------Starting Training---------
 echo $CUDA_AVAILABLE_DEVICES
-srun --qos=overcap python train.py --dataroot /project/aksoy-lab/Sepideh/data_big --name  cycle_toambient_lr2_mahdi_resnet12_linear_newData_mahdi_512_random --model cycle_pix2pix_lab --direction AtoB --display_id -1 --dataset_mode greatrandom --midas 1 --midas_normal 0 --midas_flash 0 --lambda_comp 0 --random 0 --netG resnet_12blocks --D_flash 0 --lambda_color_uv 0
+srun --qos=overcap python train.py --dataroot /project/aksoy-lab/Sepideh/data_big --name  cycle_toambient_lr2_mahdi_resnet12_linear_newData_mahdi_512_random_fix1 --model cycle_pix2pix_lab --direction AtoB --display_id -1 --dataset_mode greatrandom --midas 1 --midas_normal 0 --midas_flash 0 --lambda_comp 0 --random 0 --netG resnet_12blocks --D_flash 1 --lambda_color_uv 0
 #srun --qos=overcap python test.py --dataroot /project/aksoy-lab/Sepideh/data --name cycle_toambient_lr2_mahdi_resnet12_linear_newData --model cycle_pix2pix_lab --direction AtoB --dataset_mode alignedlab --midas 0 --midas_normal 0 --random 0  --lambda_comp 0 --netG resnet_12blocks --D_flash 0
