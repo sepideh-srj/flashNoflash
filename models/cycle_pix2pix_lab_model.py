59e6a56999410dda3ac6335aac23da8495cdbff7
ssarajia <ssarajia@solar.planets>
2021-02-05 11:57:19 -0800

    ratio

--- models/cycle_pix2pix_lab_model.py
+++ models/cycle_pix2pix_lab_model.py
@@ -36,8 +36,8 @@ class CyclePix2PixLabModel(BaseModel):
         if is_train:
             parser.set_defaults(pool_size=0, gan_mode='vanilla')
             parser.add_argument('--lambda_L1', type=float, default=1000.0, help='weight for L1 loss')
-            parser.add_argument('--lambda_A', type=float, default=1000.0, help='weight for cycle loss (A -> B -> A)')
-            parser.add_argument('--lambda_B', type=float, default=1000.0, help='weight for cycle loss (B -> A -> B)')
+            parser.add_argument('--lambda_A', type=float, default=100.0, help='weight for cycle loss (A -> B -> A)')
+            parser.add_argument('--lambda_B', type=float, default=100.0, help='weight for cycle loss (B -> A -> B)')
             parser.add_argument('--cycle_epoch', type=float, default=30, help='')
         return parser
 
@@ -56,7 +56,6 @@ class CyclePix2PixLabModel(BaseModel):
             self.loss_names += ['D_A', 'D_B']
         visual_names_B = ['real_A', 'fake_A_output']
         visual_names_A = ['real_B', 'fake_B_output']
-        visual_names_C = ['real_C', 'A_comp', 'B_comp']
         self.visual_names = visual_names_B + visual_names_A #+ visual_names_C  # combine visualizations for A and B
 
         if self.isTrain:
@@ -122,6 +121,7 @@ class CyclePix2PixLabModel(BaseModel):
         # print(self.real_A.shape)
         self.real_A_copy = input['A' if AtoB else 'B'].to(self.device)
         self.real_B = input['B' if AtoB else 'A'].to(self.device)
+        # self.flash = input['flash'].to(self.device)
         self.image_paths = input['A_paths' if AtoB else 'B_paths']
         # if self.opt.midas_normal:
         #     self.midas_A_normal = input['midas_A_normal' if AtoB else 'midas_B_normal'].to(self.device)
@@ -171,18 +171,34 @@ class CyclePix2PixLabModel(BaseModel):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         if self.opt.midas or self.opt.midas_flash or self.opt.midas_normal:
             self.A_midas = torch.cat((self.real_A, self.midas_A), 1)
-            fake_B = self.netG_A(self.A_midas)  # G_A(A)
-            self.fake_B_output = (2 * (self.real_A * fake_B + 2 * fake_B + self.real_A) - 1) / 5
+            # self.ratio_B = ((2*(self.real_A +3))/(3*(self.real_B + 3))) - (1/3)
+            # print(torch.max(self.ratio_B))
+            # print(torch.min(self.ratio_B))
+            # self.ratio_A = ((2*(self.real_B +3))/(3*(self.real_A + 3))) - (1/3)
+            # self.fake_A_output = (3*(self.ratio_A* self.real_A) + self.real_A + 9*(self.ratio_A) -3 ) / 2
+            # print(torch.max(self.fake_A_output))
+            # print(torch.min(self.fake_A_output))
+            # print(torch.max(self.ratio_A))
+            # print(torch.min(self.ratio_A))
+            self.ratio_B = self.netG_A(self.A_midas)  # G_A(A)
+            # self.fake_B_output = (2 * (self.real_A * self.ratio_B + 2 * self.ratio_B + self.real_A) - 1) / 5
+            self.fake_B_output = (3*(self.ratio_B* self.real_A) + self.real_A + 9*(self.ratio_B) -3 ) / 2
+            # print(torch.max(self.fake_B_output))
+            # print(torch.min(self.fake_B_output))
             self.fake_B_output_midas = torch.cat((self.fake_B_output, self.midas_A), 1)
             self.rec_A_ratio = self.netG_B(self.fake_B_output_midas)  # G_B(G_A(A))
-            self.rec_A = (2 * (self.fake_B_output * self.rec_A_ratio + 2 * self.rec_A_ratio + self.fake_B_output) - 1) / 5
+            self.rec_A = (3*(self.rec_A_ratio* self.fake_B_output) + self.fake_B_output + 9*(self.rec_A_ratio) -3 ) / 2
 
             self.real_B_midas = torch.cat((self.real_B, self.midas_B), 1)
-            self.fake_A = self.netG_B(self.real_B_midas)  # G_B(B)
-            self.fake_A_output = (2 * (self.real_B * self.fake_A + 2 * self.fake_A + self.real_B) - 1) / 5
+            self.ratio_A = self.netG_B(self.real_B_midas)  # G_B(B)
+            # self.fake_A_output = (2 * (self.real_B * self.fake_A + 2 * self.fake_A + self.real_B) - 1) / 5
+            self.fake_A_output = (3*(self.ratio_A* self.real_B) + self.real_B + 9*self.ratio_A -3) / 2
+            # print(torch.max(self.fake_A_output))
+            # print(torch.min(self.fake_A_output))
             self.fake_A_output_midas = torch.cat((self.fake_A_output, self.midas_B), 1)
             self.rec_B_ratio = self.netG_A(self.fake_A_output_midas)  # G_A(G_B(B))
-            self.rec_B = (2 * (self.fake_A_output * self.rec_B_ratio + 2 * self.rec_B_ratio + self.fake_A_output) - 1) / 5
+            # self.rec_B = (2 * (self.fake_A_output * self.rec_B_ratio + 2 * self.rec_B_ratio + self.fake_A_output) - 1) / 5
+            self.rec_B = (3*(self.rec_B_ratio* self.fake_A_output) + self.fake_A_output + 9*(self.rec_B_ratio) -3 ) / 2
 
             if self.opt.ratio:
                 self.A_midas = torch.cat((self.real_A, self.midas_A), 1)
